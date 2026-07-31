84e85a06c3af91bf5e022b4f445ef419c0b34b21
GiaInetello <gianellainetello@gmail.com>
2026-07-31 02:41:08 -0300

    Parte 1 terminada

--- src/tp.pl
+++ src/tp.pl
@@ -1,3 +1,4 @@
+%Punto 1a
 habitante(denken, humano, 1290, auberst).
 habitante(voll, enano, 1200, ende).
 habitante(serie, elfo, 500, weise).
@@ -10,6 +11,7 @@ habitante(lernen, humano, 1315, auberst).
 habitante(frieren, elfo, 100, weise).
 habitante(eisen, enano, 1150, riegel).
 
+%Punto 1b
 estaVivo(Persona, Anio):-
     habitante(Persona, elfo, AnioNacio, _),
     Anio>AnioNacio.
@@ -26,59 +28,120 @@ estaVivo(Persona, Anio):-
 
 %Punto 2
 %version, hazania, quien la realizo, donde
-versionHazania(unica, rescatarALaHermanaDeWirbel, stark, klares).
-versionHazania(unica, rescatarALaHermanaDeWirbel, fern, klares).
-versionHazania(versionLawine, destruirAlDemonioAura, frieren, weise).
-versionHazania(versionVoll, destruirAlDemonioAura, denken, auberst).
-versionHazania(unica, destruirAlReyDemonio, frieren, ende).
-versionHazania(unica, destruirAlReyDemonio, himmel, ende).
-versionHazania(unica, destruirAlReyDemonio, heiter, ende).
-versionHazania(unica, destruirAlReyDemonio, eisen, ende).
-versionHazania(unica, recuperarAlGatoPerdido, himmel, weise).
-versionHazania(unica, recuperarAlGatoPerdido, frieren, weise).
+versionHazania(rescatarALaHermanaDeWirbel, stark, klares).
+versionHazania(rescatarALaHermanaDeWirbel, fern, klares).
+versionHazania(version(destruirAlDemonioAura, lawine), frieren, weise).
+versionHazania(version(destruirAlDemonioAura, voll), denken, auberst).
+versionHazania(destruirAlReyDemonio, frieren, ende).
+versionHazania(destruirAlReyDemonio, himmel, ende).
+versionHazania(destruirAlReyDemonio, heiter, ende).
+versionHazania(destruirAlReyDemonio, eisen, ende).
+versionHazania(recuperarAlGatoPerdido, himmel, weise).
+versionHazania(recuperarAlGatoPerdido, frieren, weise).
 
 %Que hazania, que version, quien la conoce, como la conocio y cuando
-conoceHazania(rescatarALaHermanaDeWirbel, unica, wirbel, presencio, 1390).
-conoceHazania(rescatarALaHermanaDeWirbel, unica, frieren, presencio, 1390).
-conoceHazania(destruirAlDemonioAura, versionLawine, lawine, escuchoCancion, 1393).
-conoceHazania(destruirAlDemonioAura, versionVoll, voll, leyoLibro(50), 1400).
-conoceHazania(destruirAlReyDemonio, unica,serie, leyoLibro(100), 1335).
-conoceHazania(recuperarAlGatoPerdido, unica, kane, presencio, 1375).
+conoceHazania(rescatarALaHermanaDeWirbel, wirbel, presencio, 1390).
+conoceHazania(rescatarALaHermanaDeWirbel, frieren, presencio, 1390).
+conoceHazania(destruirAlDemonioAura, lawine, escuchoCancion, 1393).
+conoceHazania(destruirAlDemonioAura, voll, leyoLibro(50), 1400).
+conoceHazania(destruirAlReyDemonio,serie, leyoLibro(100), 1335).
+conoceHazania(recuperarAlGatoPerdido, kane, presencio, 1375).
 
+%Punto 2a
 esRecordadaEnUnAnio(Persona, Anio, Hazania):-
     estaVivo(Persona, Anio),
-    conoceHazania(Hazania, _, Persona, presencio, AnioPresencio),
+    conoceHazania(Hazania, Persona, presencio, AnioPresencio),
     Anio >= AnioPresencio.
 esRecordadaEnUnAnio(Persona, Anio, Hazania):-
     estaVivo(Persona, Anio),
-    conoceHazania(Hazania, _, Persona, escuchoCancion, AnioEscucho),
+    conoceHazania(Hazania, Persona, escuchoCancion, AnioEscucho),
     Anio >= AnioEscucho,
     Anio =< (AnioEscucho + 15).
 esRecordadaEnUnAnio(Persona, Anio, Hazania):-
     estaVivo(Persona, Anio),
-    conoceHazania(Hazania, _, Persona, leyoLibro(Paginas), AnioLeyo),
+    conoceHazania(Hazania, Persona, leyoLibro(Paginas), AnioLeyo),
     Anio >= AnioLeyo,
     Anio =< (AnioLeyo + Paginas).
+esRecordadaEnUnAnio(Persona,Anio,Hazania):-
+    estaVivo(Persona,Anio),
+    conocePorDiaFestivo(Persona,Hazania,Desde),
+    Anio >= Desde.
+esRecordadaEnUnAnio(Persona,Anio,Hazania):-
+    estaVivo(Persona,Anio),
+    conocePorEstatua(Persona,Hazania,Desde,Pueblo),
+    Anio >= Desde,
+    estatuaVigente(Pueblo,Hazania,Anio).
 
 %Punto 2b
 estaCorroborada(Hazania):-
-    conoceHazania(Hazania, _, _, _, _),  %para que sea inversible
     not(tieneVersionesDistintas(Hazania)). 
 
 tieneVersionesDistintas(Hazania):-
-    versionHazania(_, Hazania, Quien1, _),
-    versionHazania(_, Hazania, Quien2, _),
+    conoceHazania(Hazania, QuienLaConoce1, _, _),
+    conoceHazania(Hazania, QuienLaConoce2, _, _),
+    QuienLaConoce1 \= QuienLaConoce2,
+    versionHazania(version(Hazania, QuienLaConoce1), Quien1, _),
+    versionHazania(version(Hazania, QuienLaConoce2), Quien2, _),
     Quien1 \= Quien2.
 
 tieneVersionesDistintas(Hazania):-
-    versionHazania(_, Hazania, _, Donde1),
-    versionHazania(_, Hazania, _, Donde2),
+    conoceHazania(Hazania, QuienLaConoce1, _, _),
+    conoceHazania(Hazania, QuienLaConoce2, _, _),
+    QuienLaConoce1 \= QuienLaConoce2,
+    versionHazania(version(Hazania, QuienLaConoce1), _, Donde1),
+    versionHazania(version(Hazania, QuienLaConoce2), _, Donde2),
     Donde1 \= Donde2.
 
 %Punto 2c
 pasoAlOlvido(Hazania, Anio):-
-    conoceHazania(Hazania, _, _, _, _),
+    conoceHazania(Hazania, _, _, _),
     not(esRecordadaEnUnAnio(_, Anio, Hazania)).
+
+%Punto3
+
+diaFestivo(weise, destruirAlReyDemonio, 1340).
+
+estatua(auberst, "el equipo de heroes", bronce, destruirAlReyDemonio, 1370).
+estatua(auberst,"el heroe del sur", marmol,destruirASchlatElOmnisciente,1340). 
+    
+mantenimiento("el equipo de heroes",1400).
+mantenimiento("el equipo de heroes",1450).
+mantenimiento("el heroe del sur",1410).
+
+duracion(bronce,15).
+duracion(marmol,30).
+
+ventanaValida(FechaInicio, Material, Anio):-
+    duracion(Material, Duracion),
+    Anio >= FechaInicio,
+    Anio =< FechaInicio + Duracion.
+
+estatuaVigente(Pueblo,Hazania,Anio):-
+    estatua(Pueblo,Nombre,Material,Hazania,Construccion),
+    ventanaValida(Construccion,Material,Anio).
+estatuaVigente(Pueblo,Hazania,Anio):-
+    estatua(Pueblo,Nombre,Material,Hazania,_),
+    mantenimiento(Nombre,FechaMantenimiento),
+    ventanaValida(FechaMantenimiento,Material,Anio).   
+
+conocePorEstatua(Persona,Hazania,Desde,Pueblo):-
+    habitante(Persona,_,Nacimiento,Pueblo),
+    estatua(Pueblo,_,_,Hazania,Inicio),
+    maximo(Nacimiento,Inicio,Desde).  
+    
+conocePorDiaFestivo(Persona,Hazania,Desde):-
+    habitante(Persona,_,Nacimiento,Pueblo),
+    diaFestivo(Pueblo,Hazania,Inicio),
+    maximo(Nacimiento,Inicio,Desde).    
+    
+
+maximo(X,Y,X):-
+    X >= Y.
+
+maximo(X,Y,Y):-
+    Y > X.    
+   
+
 
 
 :- begin_tests(tpIntegrador, []).
@@ -111,8 +174,8 @@ pasoAlOlvido(Hazania, Anio):-
         esRecordadaEnUnAnio(wirbel, 1430, rescatarALaHermanaDeWirbel).
     test(wirbel_no_recuerda_rescatarALaHermanaDeWirbel_en_1440_porque_ya_no_esta_vivo, nondet):-
         not(esRecordadaEnUnAnio(wirbel, 1440, rescatarALaHermanaDeWirbel)).
-    %test(rescatar_a_la_hermana_de_Wirbel_es_una_hazania_corroborada, nondet):-
-    %    estaCorroborada(rescatarALaHermanaDeWirbel).
+    test(rescatar_a_la_hermana_de_Wirbel_es_una_hazania_corroborada, nondet):-
+        estaCorroborada(rescatarALaHermanaDeWirbel).
     test(destruir_al_demonio_aura_no_es_hazania_corroborada, nondet):-
         not(estaCorroborada(destruirAlDemonioAura)).
     test(destruir_al_demonio_aura_paso_al_olvido_en_1460, nondet):-
@@ -121,4 +184,18 @@ pasoAlOlvido(Hazania, Anio):-
         not(pasoAlOlvido(destruirAlDemonioAura, 1440)).
     
 
-:- end_tests(tpIntegrador).
+    %punto 3
+   
+
+    test(lawine_recuerda_destruir_al_rey_demonio_por_estatua_en_1400, nondet):-
+        esRecordadaEnUnAnio(lawine, 1400, destruirAlReyDemonio).
+
+
+    test(lawine_no_recuerda_destruir_al_rey_demonio_en_1390_por_estatua, nondet):-
+        not(esRecordadaEnUnAnio(lawine, 1390, destruirAlReyDemonio)).
+
+
+    test(fern_recuerda_destruir_al_rey_demonio_por_dia_festivo_en_1400, nondet):-
+        esRecordadaEnUnAnio(fern, 1400, destruirAlReyDemonio).
+
+    :- end_tests(tpIntegrador).
